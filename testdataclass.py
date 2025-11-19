968622c8f7e31bbc0fb7dd1dab4ae3ede32d7e70
tkwang657 <87434756+tkwang657@users.noreply.github.com>
2025-11-19 13:12:04 -0500

    test

--- testdataclass.py
+++
@@ -1,6 +0,0 @@
-filepath="datasets/2024_lar.txt"
-loader=AutoTabularLoader(file_path=filepath, chunksize=50000)
-loader.print_info()
-print(loader.df)
-loader['activity_year', 'action_taken', 'lei']
-
